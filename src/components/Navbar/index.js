f89c6b61f263f010a7ecef3d20c19b5d1c94b801
Morgan Segura <morgansegura@gmail.com>
2019-01-15 15:11:40 -0800

    Fixes an error with menus. Relocated custome styles to Site Settings

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,20 +1,16 @@
-import React from "react"
+import React, { Component } from "react"
 import CustomLink from "../CustomLink"
 
-const Navbar = ({ menuItems }) => {
+class Navbar extends Component {
 
-    const toggleMobileNav = () => {
-        const wrapper = document.getElementById("wrapper")
+    constructor(props) {
+        super(props)
 
-        // toggle open/closed calsses for transition effects
-        const superToggle = (element, class0, class1) => {
-            element.classList.toggle(class0)
-            element.classList.toggle(class1)
-        }
-
-        superToggle(wrapper, 'mobile-nav--is-open', 'mobile-nav--is-closed')
+        this.toggleMobileNav = this.toggleMobileNav.bind(this)
     }
-    const initHeader = () => {
+
+    componentDidMount() {
+
         const wrapper = document.getElementById("wrapper");
 
         window.onscroll = (e) => {
@@ -29,35 +25,53 @@ const Navbar = ({ menuItems }) => {
                     header.classList.add("unfill");
                 }
             }
-        // AOS
-        // AOS.init()
+            // AOS
+            // AOS.init()
         }
+   
     }
-    initHeader()
-    return (
-      <nav className="nav nav__main">  
-        {!!menuItems ?
-        // Note: Make the hamburger menu show if user add more than 5 links
-        // Think about allowing users to create new menus and swap slocations
-            <div className="nav__inner">
-                {menuItems.map(menuItem => (
-                    <CustomLink
-                        key={menuItem.linkURL}
-                        activeClassName="active"
-                        linkType={menuItem.linkType}
-                        linkURL={menuItem.linkURL}
-                        className="nav__item"
-                    >
-                        {menuItem.label}
-                    </CustomLink>
-                ))}
-            </div>
-        : null }        
-        <div className="nav__trigger" onClick={toggleMobileNav}>
-          <div className="nav__trigger--inner"></div>
-        </div>
-      </nav>
-    )
+
+    toggleMobileNav() {
+        const wrapper = document.getElementById("wrapper")
+
+        // toggle open/closed calsses for transition effects
+        const superToggle = (element, class0, class1) => {
+            element.classList.toggle(class0)
+            element.classList.toggle(class1)
+        }
+
+        superToggle(wrapper, 'mobile-nav--is-open', 'mobile-nav--is-closed')
+    }    
+
+    render() {
+
+        console.log(this.props)
+
+        return (
+            <nav className="nav nav__main">
+                {!!this.props.menuItems ?
+                    // Note: Make the hamburger menu show if user add more than 5 links
+                    // Think about allowing users to create new menus and swap slocations
+                    <div className="nav__inner">
+                        {this.props.menuItems.map(menuItem => (
+                            <CustomLink
+                                key={menuItem.linkURL}
+                                activeClassName="active"
+                                linkType={menuItem.linkType}
+                                linkURL={menuItem.linkURL}
+                                className="nav__item"
+                            >
+                                {menuItem.label}
+                            </CustomLink>
+                        ))}
+                    </div>
+                    : null}
+                <div className="nav__trigger" onClick={toggleMobileNav}>
+                    <div className="nav__trigger--inner"></div>
+                </div>
+            </nav>
+        )        
+    }
 }
 
 export default Navbar 
